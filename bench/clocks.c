19a550974b1d5bd34b1c4d34790b56ce09eb7b0c
armfazh <armfazh@gmail.com>
2025-02-19 01:15:48 -0800

    Adding exhibit A from MPL

--- bench/clocks.c
+++ bench/clocks.c
@@ -1,3 +1,13 @@
+/**
+ * hpke-simdium
+ * Copyright 2025 Armando Faz Hernandez.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
 #include "clocks.h"
 #include <sys/time.h>
 #include <time.h>
--- dhkem_avx2.h
+++ dhkem_avx2.h
@@ -1,13 +1,10 @@
 /**
- * This file is part of hpke-simdium.
- *
+ * hpke-simdium
  * Copyright 2025 Armando Faz Hernandez.
  *
- * Licensed under the Mozilla Public License, v. 2.0. You may not use this
- * file except in compliance with the License.
- * You can obtain a copy of the License at:
- *
- * https://www.mozilla.org/en-US/MPL/2.0/
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
  * SPDX-License-Identifier: MPL-2.0
  */
