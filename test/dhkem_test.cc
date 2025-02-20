b8edf9ad3c7c1f066061ea5fdbc303ca8ad2485d
armfazh <armfazh@gmail.com>
2025-02-20 06:26:14 -0800

    add test for dhkem

---
+++ test/dhkem_test.cc
@@ -0,0 +1,96 @@
+/**
+ * hpke-simdium
+ * Copyright 2025 Armando Faz Hernandez.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+#include <gtest/gtest.h>
+#include <dhkem.h>
+
+extern testing::AssertionResult u8_cmp(const char *a_expr,
+                                       const char *b_expr, u8 * a, u8 * b);
+
+struct dhkem_test_vector
+{
+    struct
+    {
+        const char *ss, *enc, *skR, *pkR;
+    } encapdecap;
+
+    struct
+    {
+        const char *ss, *enc, *skR, *pkR, *skS, *pkS;
+    } auth_encapdecap;
+};
+
+typedef testing::TestWithParam<dhkem_test_vector> DHKEM;
+
+TEST_P(DHKEM, encapdecap)
+{
+    dhkem_test_vector v = GetParam();
+    u8 enc = u8_hex_string(v.encapdecap.enc);
+    u8 skR = u8_hex_string(v.encapdecap.skR);
+    u8 pkR = u8_hex_string(v.encapdecap.pkR);
+    u8 ss_want = u8_hex_string(v.encapdecap.ss);
+
+    u8_static(ss_encap, 32);
+    encap(&ss_encap, &enc, &pkR);
+    EXPECT_PRED_FORMAT2(u8_cmp, &ss_encap, &ss_want);
+
+    u8_static(ss_decap, 32);
+    decap(&ss_decap, &enc, &skR, &pkR);
+    EXPECT_PRED_FORMAT2(u8_cmp, &ss_decap, &ss_want);
+
+    u8_free(&enc);
+    u8_free(&skR);
+    u8_free(&pkR);
+    u8_free(&ss_want);
+}
+
+TEST_P(DHKEM, auth_encapdecap)
+{
+    dhkem_test_vector v = GetParam();
+    u8 enc = u8_hex_string(v.auth_encapdecap.enc);
+    u8 skR = u8_hex_string(v.auth_encapdecap.skR);
+    u8 pkR = u8_hex_string(v.auth_encapdecap.pkR);
+    u8 skS = u8_hex_string(v.auth_encapdecap.skS);
+    u8 pkS = u8_hex_string(v.auth_encapdecap.pkS);
+    u8 ss_want = u8_hex_string(v.auth_encapdecap.ss);
+
+    u8_static(ss_encap, 32);
+    auth_encap(&ss_encap, &enc, &pkR, &skS, &pkS);
+    EXPECT_PRED_FORMAT2(u8_cmp, &ss_encap, &ss_want);
+
+    u8_static(ss_decap, 32);
+    auth_decap(&ss_decap, &enc, &skR, &pkR, &pkS);
+    EXPECT_PRED_FORMAT2(u8_cmp, &ss_decap, &ss_want);
+
+    u8_free(&enc);
+    u8_free(&skR);
+    u8_free(&pkR);
+    u8_free(&skS);
+    u8_free(&pkS);
+    u8_free(&ss_want);
+}
+
+INSTANTIATE_TEST_CASE_P(, DHKEM, testing::Values(dhkem_test_vector
+{
+    .encapdecap = {
+        .ss = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .enc = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .skR = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .pkR = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+    },
+    .auth_encapdecap = {
+        .ss = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .enc = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .skR = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .pkR = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .skS = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+        .pkS = "88981076b632a816d78522e1e51993b583988288850e506062465cb31bd46366",
+    },
+}));
--- test/kdf_test.cc
+++ test/kdf_test.cc
@@ -11,38 +11,8 @@
 #include <gtest/gtest.h>
 #include <kdf.h>
 
-static std::string u8_to_string(u8 *x)
-{
-    std::stringstream s;
-    char str[3] = {0};
-
-    for (size_t i = 0; i < x->len; i++)
-    {
-        sprintf(str, "%02x", x->data[i]);
-        s << str ;
-    };
-
-    return s.str();
-}
-
-static testing::AssertionResult u8_cmp(
-    const char *a_expr,
-    const char *b_expr,
-    u8 * a,
-    u8 * b)
-{
-    if (a->len == b->len && memcmp(a->data, b->data, a->len) == 0)
-    {
-        return testing::AssertionSuccess();
-    }
-    else
-    {
-        return testing::AssertionFailure()
-               << "mismatch " << a_expr << " and " << b_expr
-               << std::endl << "got:  (" << a->len << ") " << u8_to_string(a)
-               << std::endl << "want: (" << b->len << ") " << u8_to_string(b);
-    }
-}
+extern testing::AssertionResult u8_cmp(const char *a_expr,
+                                       const char *b_expr, u8 * a, u8 * b);
 
 struct kdf_test_vector
 {
